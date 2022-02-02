3b26e61a3e12b65795fa5f5edf03de238d98def2
ShamsRahman-Groundwater <98888462+ShamsRahman-Groundwater@users.noreply.github.com>
2022-02-02 12:45:42 +0000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+Running the code:
+1. Launch MATLAB
+2. Open run_GW_model.m in MATLAB editor.
+3. Run.
---
+++ run_GW_model.m
@@ -0,0 +1,35 @@
+% Author: Mostaquimur Rahman, University of Bristol, UK
+% (ar15645@bristol.ac.uk)
+% This script executes the groundwater model code groundwater.m
+
+clear
+close all
+clc
+
+topography = load('test_topography.txt'); % topographic height (m)
+topo_resolution = 1000; % topographic resolution (m)
+recharge = load('test_recharge.txt'); % groundwater recharge (m/d)
+Sy = load('test_Sy.txt'); % specific yield (-)
+T = load('test_T.txt'); % transmissivity (m2/d)
+ini_WTD = 100; % initial groundwater table depth (m below surface)
+dt = 1; % time step (d)
+total_sim_steps = 1000; % total simulation steps
+
+Hold = topography - ini_WTD; % initial groundwater level (m)
+for t = 1:total_sim_steps   
+    % call groundwater model   
+    [Hnew,runoff_md] = groundwater(topography,topo_resolution, ...
+                                   recharge,Sy,T,Hold,dt);
+    
+    Hold = Hnew;
+    head_mOB = Hnew;   
+    disp(['Timestep ' num2str(t) ' complete'])
+end
+% outputs: head_mOB = groundwater head elevation (m)
+%          runoff_md = runoff in m/d
+save('results.mat','head_mOB','runoff_md')
+
+% plot groundwater table depth
+figure
+imagesc(topography-head_mOB)
+colorbar
